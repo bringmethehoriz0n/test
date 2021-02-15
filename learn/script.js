a76e3e900ec7e0020fbc01e5284bc75dce2345d3
unknown <pixelweb@inbox.ru>
2021-02-15 12:42:24 +0300

    Это первый комит

---
+++ learn/script.js
@@ -0,0 +1,46 @@
+    'use strict';
+
+// var leftBorderWidht = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+
+// let person = {
+//     name: 'John',
+//     age: '25',
+//     isMarried: false
+// };
+
+// console.log(person.name);
+// console.log(person['age']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+// alert ('hello world');
+// let answer = confirm ('Are you here?');
+// console.log(answer);
+
+
+// let answer = +prompt ('Есть ли вам 18 лет?', 'Да');
+// console.log(typeof(answer));
+
+// console.log('arr' + '- object');
+// console.log(4 + '- object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log('2' == 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
